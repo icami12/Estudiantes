d6938244e02bed8f0da28f23c22183de2d49444f
Camila Irala <iralaca12@gmail.com>
2024-10-27 22:10:32 -0300

    superuser y manejo del panel de administración de Django

--- gestor_estudiantes/estudiante/admin.py
+++ gestor_estudiantes/estudiante/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Estudiante, Curso
 
-# Register your models here.
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cantidad_horas')  
+    search_fields = ('nombre', 'cantidad_horas')
+
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'curso_alumn', 'nota_curso') 
+    search_fields = ('nombre', 'apellido', 'curso_alumn')
+    
+admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Curso, CursoAdmin)
